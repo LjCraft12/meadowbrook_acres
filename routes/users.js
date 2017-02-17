888707f921ba5e08f425130b47f8970a1d6e7de1
Perry Craft <LjCraft12@gmail.com>
2017-02-17 01:56:38 -0500

    problem with posting new user vid 13:31

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,27 @@
-const express = require('express');
-const router  = express.Router();
+const express  = require('express');
+const router   = express.Router();
+const passport = require('passport');
+const jwt      = require('jsonwebtoken');
+const User     = require('../models/user');
+
 
 // Register Route
-router.get('/register', (req, res, next) => {
-    res.send('Hello from register');
+router.post('/register', (req, res, next) => {
+    let newUser = new User({
+        name:     req.body.name,
+        email:    req.body.email,
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    User.addUser(newUser,  (err, user) => {
+        if (err) {
+            res.json({ success: false, message: 'Failed to register user' });
+        } else {
+            res.json({ success: true, message: 'User created successfully' })
+        }
+    });
+
 });
 
 // Authenticate Route
@@ -14,11 +32,6 @@ router.post('/authenticate', (req, res, next) => {
 // Profile Route
 router.get('/profile', (req, res, next) => {
     res.send('Hello from profile');
-});
-
-// Validate Route
-router.get('/validate', (req, res, next) => {
-    res.send('Hello from validate');
 });
 
 module.exports = router;
