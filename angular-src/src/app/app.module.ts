253c0c3c224471c158a80a725dc87f01548c548d
Perry Craft <LjCraft12@gmail.com>
2017-02-24 02:48:14 -0500

    setup angular 2 cli and added routing to all pages

---
+++ angular-src/src/app/app.module.ts
@@ -0,0 +1,48 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { CommunityComponent } from './components/community/community.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { RouterModule, Routes} from  '@angular/router';
+import { SecurityComponent } from './components/security/security.component';
+
+const appRoutes: Routes = [
+  { path:'', component: HomeComponent },                                                                                // Setting up the path for the homepage
+  { path:'register', component: RegisterComponent },                                                                    // Setting up Register route
+  { path:'login', component: LoginComponent },                                                                          // Setting up Login route
+  { path:'dashboard', component: DashboardComponent },                                                                  // Setting up Dashboard route
+  { path:'community', component: CommunityComponent },                                                                  // Setting up Community route
+  { path:'profile', component: ProfileComponent },                                                                      // Setting up Profile route
+  { path:'security', component: SecurityComponent },                                                                    // Setting up Security route
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    CommunityComponent,
+    DashboardComponent,
+    ProfileComponent,
+    SecurityComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
