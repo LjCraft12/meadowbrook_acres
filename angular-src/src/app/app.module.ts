7c63f1756d2110e993ac3f25dfac77dfc54b1b86
Perry Craft <LjCraft12@gmail.com>
2017-02-25 18:56:49 -0500

    user registration works and redirects to login page.

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -16,6 +16,7 @@ import { SecurityComponent } from './components/security/security.component';
 
 // Custom services
 import { ValidateService } from './services/validate.service';
+import { AuthService } from './services/auth.service';
 import { FlashMessagesModule } from 'angular2-flash-messages';
 
 const appRoutes: Routes = [
@@ -47,7 +48,7 @@ const appRoutes: Routes = [
     RouterModule.forRoot(appRoutes),
     FlashMessagesModule
   ],
-  providers: [ValidateService],
+  providers: [ValidateService, AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
