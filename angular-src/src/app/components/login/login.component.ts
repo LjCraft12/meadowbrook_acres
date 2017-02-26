96c6a2f3804b626f3ee1f517a30e1439a42dc8bc
Perry Craft <LjCraft12@gmail.com>
2017-02-25 19:28:51 -0500

    setting up login auth service

--- angular-src/src/app/components/login/login.component.ts
+++ angular-src/src/app/components/login/login.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+  username: String;
+  password: String;
   constructor() { }
 
   ngOnInit() {
   }
 
+  onLoginSubmit() {
+    const user = {                                                                                                      // User object to be submitted to the backend auth service
+      username: this.username,
+      password: this.password
+    }
+  }
 }
